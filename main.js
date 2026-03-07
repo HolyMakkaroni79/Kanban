d7d68c091618f70363e1e1c65cecce65d19c455a
HolyMakkaroni79 <Toni.Kordik@gmail.com>
2026-03-08 00:14:27 +0100

    js for a drag and drop mechanic
    unable to drop.
    no comments
    (no understanding)

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 let items = document.getElementsByClassName('item');
+let leftBox = document.querySelector('.left');
+let rightBox = document.querySelector('.right');
+
+for(items of items){
+    items.addEventListener("dragstart", (e) =>
+    {selected = e.targed});
+}
+
+rightBox.addEventListener("dragover", (e) =>{
+    e.preventDefault();
+})
+
+rightBox.addEventListener('drop', (e) => {
+    if(selected){
+        rightBox.appendChild(selected);
+        selected = null;
+    }
+});
+
+leftBox.addEventListener("dragover", (e) =>{
+    e.preventDefault();
+})
+
+leftBox.addEventListener('drop', (e) => {
+    if(selected){
+        leftBox.appendChild(selected);
+        selected = null;
+    }
+});
