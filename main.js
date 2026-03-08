59435649f5839683b62c01a4dcd4da4e45426ba9
HolyMakkaroni79 <Toni.Kordik@gmail.com>
2026-03-08 12:59:14 +0100

    adding comments
    (many comments...)

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-let items = document.getElementsByClassName('item');
-let leftBox = document.querySelector('.left');
-let rightBox = document.querySelector('.right');
+let items = document.getElementsByClassName('item'); //all items (some kind of list)
+let leftBox = document.querySelector('.left');   //one element (no list)
+let rightBox = document.querySelector('.right'); //.for class name bc you can serach for more than only for classes (.name is a class)
 
-for(items of items){
-    items.addEventListener("dragstart", (e) =>
-    {selected = e.targed});
-}
+for(items of items){ //for each element of the (list) "items"
+    items.addEventListener("dragstart", (e) => //attaching event listener to the object
+    {selected = e.target}); //e is the event object
+}; //for each wants a tiny simicolon :)
 
-rightBox.addEventListener("dragover", (e) =>{
-    e.preventDefault();
-})
+rightBox.addEventListener("dragover", (e) =>{ //dragover fires while element is dragged
+    e.preventDefault(); //tells the browser he have to chill even if an item gets dragged away
+}); //events for normal too
 
-rightBox.addEventListener('drop', (e) => {
+rightBox.addEventListener('drop', (e) => { //define eventnames as string (there is no difference between 'drop' and "drop")
     if(selected){
         rightBox.appendChild(selected);
         selected = null;
-    }
+    } //well js say´s that a for each is a statement and need a semicolon but a if(statement) don´t need one, bc its a "control structure"
 });
 
 leftBox.addEventListener("dragover", (e) =>{
     e.preventDefault();
-})
+}) //but if there have none simicolon, js will add one where it expect one to be
 
 leftBox.addEventListener('drop', (e) => {
     if(selected){
         leftBox.appendChild(selected);
         selected = null;
     }
-});
+}); //but I will be consistent with the semicolon 
